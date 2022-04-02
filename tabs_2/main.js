309eebc7a5be453fe14950af689e8df271f8d9f6
lyhaonhien <101444507+lyhaonhien@users.noreply.github.com>
2022-04-02 08:37:28 +0700

    Add files via upload

---
+++ tabs_2/main.js
@@ -0,0 +1,16 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    /*Hide all tabcontents*/
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName('tablinks');
+    /*Remove class "active" from tablinks*/
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(' active', '');
+    }
+    /*Add class "active" to current tablink and display current tabcontent*/
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
